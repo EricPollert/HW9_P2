92e378e3bb7d1af441052e8ca200666efe15ce99
EricPollert <112413919+EricPollert@users.noreply.github.com>
2022-10-24 03:34:17 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@HW9P2/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+  
+  const int COL_NUM = 5;
+  const int ROW_NUM = 5;
+  int grade [ROW_NUM][COL_NUM] = { {100, 100, }};
+
+  for (int r = 0; r < ROW_NUM; r++){
+  for (int c = 0; c < COL_NUM; c++){
+    cout << setw(5) << grade[r][c];
+    
+    }
+    int avg = (grade[r][1] + grade[r][2] + grade[r][3] + grade[r][4]) / 4;
+    cout << setw(5) << avg;
+    int avg1 = (grade[r][1] * 0.2 + grade[r][2] * 0.3 + grade[r][3] * 0.3 + grade[r][4] * 0.2) / 4;
+    cout << setw(5) << avg1;
+    
+    cout << endl;
+  }
+  
+}
